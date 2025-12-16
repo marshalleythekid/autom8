92eb981f64488255eac0aaa20ab0f4636fc1ec45
unknown <leeymarshall@gmail.com>
2025-12-16 14:58:51 +0700

    testing a new code

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -29,5 +29,9 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "linebreak-style": "off",
+  "object-curly-spacing": "off",
+  "max-len": "off",
+  "@typescript-eslint/no-explicit-any": "off"
   },
 };
